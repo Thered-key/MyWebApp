65bf40904f13c8791731f95ce5ef0bd33480b2f4
googosoft <googosoft@googosoftdeiMac-3.local>
2019-04-11 09:00:54 +0800

    第四天

---
+++ www/test.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# _*_ coding:utf-8 _*_
+import asyncio
+import orm
+from models import User, Blog, Comment, next_id
+
+async def test(loop):
+	await orm.create_pool(loop=loop, user='root', password='goo_900_',db='yhc')
+	u = User(name='Test', email='test@exa.com', passwd='qweasd', image='about:blank')
+	await u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.run_forever()
+
+for x in test(loop):
+	pass
